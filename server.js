b94bb9e2e292e5cb6cf3023d0e1e39e0f483971a
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-05-04 13:04:35 +0530

    added 3623 files

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`API REQUEST PATH : ${req.path}`);
+  next();
+});
+
+app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+//example <Route path='' component={component}/>
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
